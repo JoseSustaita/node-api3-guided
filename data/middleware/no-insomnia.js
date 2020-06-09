ac6fa98394694ec0f8eee8a48aa204622380f1a3
Jose Sustaita <josesustaita70@gmail.com>
2020-06-08 22:18:57 -0400

    Lecture notes

---
+++ data/middleware/no-insomnia.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  return (req, res, next) => {
+    // a stromg that tells us what software the client is using
+    const agent = req.headers["user-agent"];
+    if (/insomnia/i.test(agent)) {
+      return res.status(418).json({
+        message: "No Insomnia allowed here",
+      });
+    }
+    next();
+  };
+};
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require("express")
-const welcomeRouter = require("./welcome/welcome-router")
-const usersRouter = require("./users/users-router")
+const express = require("express");
+const morgan = require("morgan");
+const welcomeRouter = require("./welcome/welcome-router");
+const usersRouter = require("./users/users-router");
+const logger = require("./data/middleware/logger");
+const noInsomnia = require("./data/middleware/no-insomnia");
 
-const server = express()
-const port = 4000
+const server = express();
+const port = 4000;
 
-server.use(express.json())
+server.use(express.json());
+// server.use(morgan("combined"));
+// server.use(noInsomnia());
+server.use(logger("long"));
 
-server.use(welcomeRouter)
-server.use(usersRouter)
+server.use(welcomeRouter);
+server.use(usersRouter);
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
